6fdbc6c4423e61a0478ea4e3b4b4b06d01393078
OrangeVulture <fruitfuljoseph@gmail.com>
2020-04-22 18:17:10 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: Center(
+        child: Text('Building a Shop!!'),
+      ),
+    );
+  }
+}
